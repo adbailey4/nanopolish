5f69df7a493f4a2ddc6109ec5af4dbe0dd3ac2b1
Andrew D Bailey IV <bailey.andrew4@gmail.com>
2020-09-03 17:12:29 -0700

    more updated cmake workflow so that all dependencies are imported via find_package

--- cmake/AddDependencies.cmake
+++ cmake/AddDependencies.cmake
@@ -1,155 +1,27 @@
 message(CHECK_START "Finding Nanopolish Dependencies")
 list(APPEND CMAKE_MESSAGE_INDENT "  ")
 ############################################################################################################
-# DL LIBRARY
+find_package(HTSlib REQUIRED htslib)
+find_package(Eigen3 3.2 REQUIRED NO_MODULE)
+find_package(OpenMP REQUIRED CXX)
+#set(HDF5_FIND_DEBUG TRUE)
+set(HDF5_PREFER_PARALLEL TRUE)
+find_package(HDF5 1.10.4 COMPONENTS C CXX HL Fortran)
 ############################################################################################################
-message(CHECK_START "Finding DL LIBRARY")
-find_library(LIBDL_LIBRARY NAMES dl)
+set(nanopolish_LINK_LIBRARIES
+        HTSlib::HTSlib
+        Eigen3::Eigen
+        ${CMAKE_DL_LIBS}
+        OpenMP::OpenMP_CXX
+        )
 
-if(NOT LIBDL_LIBRARY)
-    message(FATAL_ERROR "dl library not found")
+if (NOT TARGET hdf5::hdf5)
+    list(APPEND nanopolish_LINK_LIBRARIES "${HDF5_LIBRARIES}")
+    set(nanopolish_INCLUDE_DIRS
+            ${HDF5_INCLUDE_DIRS})
 else()
-    message(CHECK_PASS "found: ${LIBDL_LIBRARY}")
+    list(APPEND nanopolish_LINK_LIBRARIES hdf5::hdf5)
 endif()
-############################################################################################################
-# ZLIB LIBRARY
-############################################################################################################
-message(CHECK_START "Finding ZLIB PACKAGE")
-find_package(ZLIB)
-if(NOT ZLIB_FOUND)
-    message(FATAL_ERROR "zlib package not found")
-else()
-    message(CHECK_PASS "found: ${ZLIB_LIBRARY}")
-endif()
-############################################################################################################
-# M LIBRARY
-############################################################################################################
-message(CHECK_START "Finding LIBM LIBRARY")
-find_library(LIBM_LIBRARY NAMES m)
-if(NOT LIBM_LIBRARY)
-    message(FATAL_ERROR "m library not found")
-else()
-    message(CHECK_PASS "found: ${LIBM_LIBRARY}")
-endif()
-############################################################################################################
-# pthread LIBRARY
-############################################################################################################
-message(CHECK_START "Finding LIBPTHREAD LIBRARY")
-find_library(LIBPTHREAD_LIBRARY NAMES pthread)
-if(NOT LIBPTHREAD_LIBRARY)
-    message(FATAL_ERROR "pthread library not found")
-else()
-    message(CHECK_PASS "found: ${LIBPTHREAD_LIBRARY}")
-endif()
-############################################################################################################
-# BZ2 LIBRARY
-############################################################################################################
-message(CHECK_START "Finding bz2 LIBRARY")
-find_library(LIBBZ2_LIBRARY NAMES bz2)
-if(NOT LIBBZ2_LIBRARY)
-    message(FATAL_ERROR "bz2 library not found")
-else()
-    message(CHECK_PASS "found: ${LIBBZ2_LIBRARY}")
-endif()
-############################################################################################################
-# CURL LIBRARY
-############################################################################################################
-message(CHECK_START "Finding curl LIBRARY")
-find_library(LIBCURL_LIBRARY NAMES curl)
-if(NOT LIBCURL_LIBRARY)
-    message(FATAL_ERROR "curl library not found")
-else()
-    message(CHECK_PASS "found: ${LIBCURL_LIBRARY}")
-endif()
-############################################################################################################
-# LZMA LIBRARY
-############################################################################################################
-message(CHECK_START "Finding lzma LIBRARY")
-find_library(LIBLZMA_LIBRARY NAMES lzma)
-if(NOT LIBLZMA_LIBRARY)
-    message(FATAL_ERROR "lzma library not found")
-else()
-    message(CHECK_PASS "found: ${LIBLZMA_LIBRARY}")
-endif()
-############################################################################################################
-# HDF LIBRARY
-############################################################################################################
-message(CHECK_START "Finding hdf5 PACKAGE")
-find_package(HDF5)
-#find_library(HDF5_LIBRARIES libhdf5.a
-#        HINTS ${NANOPOLISH_HOME}/lib
-#        ${CMAKE_INSTALL_PREFIX}/lib
-#        /usr/lib
-#        ${LIBDIR})
-#
-#find_path(HDF5_INCLUDE_DIRS hdf5.h
-#        HINTS ${HDF5_LIBRARIES}
-#        "${NANOPOLISH_HOME}/include"
-#        ${CMAKE_INSTALL_PREFIX}/include
-#        ${INCLUDEDIR}
-#        /usr/include)
-if(NOT HDF5_FOUND)
-    message(FATAL_ERROR "hdf5 package not found")
-else()
-    message(CHECK_PASS "found: ${HDF5_LIBRARIES} ${HDF5_INCLUDE_DIRS}")
-endif()
-############################################################################################################
-# EIGEN LIBRARY
-############################################################################################################
-message(CHECK_START "Finding eigen3 PACKAGE")
-find_package(eigen3)
-if(NOT eigen3_FOUND)
-    message(FATAL_ERROR "eigen3 package not found")
-else()
-    message(CHECK_PASS "found: ${EIGEN3_INCLUDE_DIR}")
-endif()
-
-#find_path(EIGEN3_INCLUDE_DIR Eigen HINTS
-#        ${CMAKE_INSTALL_PREFIX}/include
-#        ${NANOPOLISH_HOME}
-#        ${INCLUDEDIR}
-#        PATH_SUFFIXES eigen3 eigen)
-############################################################################################################
-# HTSLIB LIBRARY
-############################################################################################################
-find_library(HTSLIB_LIBRARY NAMES libhts.a
-        HINTS "${NANOPOLISH_HOME}/htslib")
-
-find_path(HTSLIB_INCLUDE_DIR htslib/hts.h
-        HINTS ${HTSLIB_LIBRARY}
-        "${NANOPOLISH_HOME}/htslib")
-
-message(CHECK_START "Finding htslib LIBRARY")
-if(NOT HTSLIB_INCLUDE_DIR OR NOT HTSLIB_LIBRARY)
-    message(FATAL_ERROR "htslib package not found")
-else()
-    message(CHECK_PASS "found: ${HTSLIB_LIBRARY} ${HTSLIB_INCLUDE_DIR}")
-endif()
-
-############################################################################################################
-# OpenMP LIBRARY
-############################################################################################################
-
-message(CHECK_START "Finding OpenMP PACKAGE")
-find_package(OpenMP)
-if(NOT TARGET OpenMP::OpenMP_CXX)
-    message(FATAL_ERROR "OpenMP package not found")
-#    find_package(Threads REQUIRED)
-#    if(NOT Threads_FOUND)
-#        message(FATAL_ERROR "OpenMP package not found")
-#    else()
-#        add_library(OpenMP::OpenMP_CXX IMPORTED INTERFACE)
-#        set_property(TARGET OpenMP::OpenMP_CXX
-#                PROPERTY INTERFACE_COMPILE_OPTIONS ${OpenMP_CXX_FLAGS})
-#        # Only works if the same flag is passed to the linker; use CMake 3.9+ otherwise (Intel, AppleClang)
-#        set_property(TARGET OpenMP::OpenMP_CXX
-#                PROPERTY INTERFACE_LINK_LIBRARIES ${OpenMP_CXX_FLAGS} Threads::Threads)
-#        message(CHECK_PASS "found: ${OpenMP_CXX_FLAGS}")
-#    endif()
-else()
-    message(CHECK_PASS "found: ${OpenMP_CXX_FLAGS}")
-endif()
-
 ############################################################################################################
 ############################################################################################################
 list(POP_BACK CMAKE_MESSAGE_INDENT)
--- cmake/nanopolishConfig.cmake
+++ cmake/nanopolishConfig.cmake
@@ -1,29 +1,20 @@
+message(CHECK_START "Finding Nanopolish Dependencies")
+list(APPEND CMAKE_MESSAGE_INDENT "  ")
+############################################################################################################
 get_filename_component(nanopolish_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(CMakeFindDependencyMacro)
 # Same syntax as find_package
+############################################################################################################
+enable_language(Fortran)
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}" "${CMAKE_CURRENT_LIST_DIR}/modules")
 find_dependency(OpenMP REQUIRED)
 find_dependency(ZLIB REQUIRED)
-#find_dependency(dl REQUIRED)
-#find_dependency(m REQUIRED)
-#find_dependency(pthread REQUIRED)
-#find_dependency(bz2 REQUIRED)
-#find_dependency(curl REQUIRED)
-#find_dependency(lzma REQUIRED)
-find_dependency(HDF5 REQUIRED)
-find_dependency(eigen3 REQUIRED)
-#find_dependency(hts REQUIRED)
-
-#if(NOT TARGET nanopolish::nanopolish_objlib)
+find_dependency(HDF5 1.10.4 COMPONENTS C CXX HL Fortran)
+find_dependency(Eigen3 3.2 REQUIRED NO_MODULE)
+find_dependency(HTSlib REQUIRED htslib)
+############################################################################################################
 include("${CMAKE_CURRENT_LIST_DIR}/nanopolishTargets.cmake")
-#endif()
+############################################################################################################
+list(POP_BACK CMAKE_MESSAGE_INDENT)
+message(CHECK_PASS "all components found")
 
-
-#get_filename_component(JSONUtils_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-#include(CMakeFindDependencyMacro)
-#
-#find_dependency(Boost 1.55 REQUIRED COMPONENTS regex)
-#find_dependency(RapidJSON 1.0 REQUIRED MODULE)
-#
-#if(NOT TARGET JSONUtils::JSONUtils)
-#    include("${JSONUtils_CMAKE_DIR}/JSONUtilsTargets.cmake")
-#endif()
