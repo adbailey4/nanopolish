4b9670936622b2d2fb95bc669a388172bb8cb3d3
Andrew D Bailey IV <bailey.andrew4@gmail.com>
2020-09-13 21:54:57 -0700

    Dockerfile and cmake adding

--- cmake/AddDependencies.cmake
+++ cmake/AddDependencies.cmake
@@ -1,12 +1,24 @@
 message(CHECK_START "Finding Nanopolish Dependencies")
 list(APPEND CMAKE_MESSAGE_INDENT "  ")
+
+IF(NOT FIND_STATIC)
+    message(STATUS "FIND_STATIC")
+    SET(CMAKE_FIND_LIBRARY_SUFFIXES .so .dylib ${CMAKE_FIND_LIBRARY_SUFFIXES})
+ELSE(FIND_STATIC)
+    message(STATUS "not FIND_STATIC")
+    SET(CMAKE_FIND_LIBRARY_SUFFIXES .a ${CMAKE_FIND_LIBRARY_SUFFIXES})
+ENDIF(NOT FIND_STATIC)
+
 ############################################################################################################
 find_package(HTSlib REQUIRED htslib)
-find_package(Eigen3 3.2 REQUIRED NO_MODULE)
+find_package(Eigen3 REQUIRED NO_MODULE)
 find_package(OpenMP REQUIRED CXX)
+
 #set(HDF5_FIND_DEBUG TRUE)
 set(HDF5_PREFER_PARALLEL TRUE)
-find_package(HDF5 1.10.4 COMPONENTS C CXX HL Fortran)
+find_package(HDF5 1.10.0 COMPONENTS C CXX REQUIRED)
+#find_package(HDF5 1.10.4 COMPONENTS C CXX)
+
 ############################################################################################################
 set(nanopolish_LINK_LIBRARIES
         HTSlib::HTSlib
@@ -19,8 +31,11 @@ if (NOT TARGET hdf5::hdf5)
     list(APPEND nanopolish_LINK_LIBRARIES "${HDF5_LIBRARIES}")
     set(nanopolish_INCLUDE_DIRS
             ${HDF5_INCLUDE_DIRS})
+    if(NOT APPLE)
+        list(APPEND nanopolish_LINK_LIBRARIES aec)
+    endif(NOT APPLE)
 else()
-    list(APPEND nanopolish_LINK_LIBRARIES hdf5::hdf5)
+list(APPEND nanopolish_LINK_LIBRARIES hdf5::hdf5)
 endif()
 ############################################################################################################
 ############################################################################################################
--- cmake/nanopolishConfig.cmake
+++ cmake/nanopolishConfig.cmake
@@ -5,11 +5,10 @@ get_filename_component(nanopolish_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(CMakeFindDependencyMacro)
 # Same syntax as find_package
 ############################################################################################################
-enable_language(Fortran)
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}" "${CMAKE_CURRENT_LIST_DIR}/modules")
 find_dependency(OpenMP REQUIRED)
 find_dependency(ZLIB REQUIRED)
-find_dependency(HDF5 1.10.4 COMPONENTS C CXX HL Fortran)
+find_dependency(HDF5 1.10.4 COMPONENTS C CXX)
 find_dependency(Eigen3 3.2 REQUIRED NO_MODULE)
 find_dependency(HTSlib REQUIRED htslib)
 ############################################################################################################
